e53c1399eb786adfec121a7951b7a072974a761d
Aart Sharma <aarti.sharmabhl@gmail.com>
2022-01-31 15:47:01 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
@@ -8,8 +7,8 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
-      {/* <Header /> */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,82 @@
 import React from "react";
+import styled from "styled-components";
+// import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <a href="#" alt="">
+          Model S
+        </a>
+
+        <a href="#" alt="">
+          Model 3
+        </a>
+
+        <a href="#" alt="">
+          Model X
+        </a>
+
+        <a href="#" alt="">
+          Model Y
+        </a>
+      </Menu>
+
+      <RightMenu>
+        <a href="#" alt="">
+          Shop
+        </a>
+        <a href="#" alt="">
+          tesla account
+        </a>
+        {/* <MenuButton /> */}
+      </RightMenu>
+    </Container>
+  );
 }
 
 export default Header;
+
+// const MenuButton = styled(MenuOpenIcon)``;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 30px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0px 10px;
+    text-wrap: nowrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin: 10px;
+  }
+`;
